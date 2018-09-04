68978e1de493c61d0e44a15a13a7ebb21f57b5c5
Harrison <frnharrison@gmail.com>
2018-09-04 10:33:29 +0200

    Initial Commit

---
+++ Find/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find
+//
+//  Created by etudiant on 04/09/2018.
+//  Copyright © 2018 WeDevTogether. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
